c96dc738296ca8e32d0f8960783f52534fa60947
Lucio Maciel <luciofm@gmail.com>
2014-12-23 20:24:36 -0200

    Fix indentation

--- app/src/main/java/com/weheartit/challenge/CommitModel.java
+++ app/src/main/java/com/weheartit/challenge/CommitModel.java
@@ -2,24 +2,24 @@ package com.weheartit.challenge;
 
 public class CommitModel {
 
-	public CommitModel() {
-		commit = new Commit();
-	}
-	
-	public Commit commit;
-	
-	public class Commit {
-		
-		public Commit() {
-			author = new Author(); 
-		}
-		
-		public String message; 
-		public String id; 
-		public Author author; 
-		
-		public class Author {
-			public String name; 
-		}
-	}
+    public CommitModel() {
+        commit = new Commit();
+    }
+
+    public Commit commit;
+
+    public class Commit {
+
+        public Commit() {
+            author = new Author();
+        }
+
+        public String message;
+        public String id;
+        public Author author;
+
+        public class Author {
+            public String name;
+        }
+    }
 }
--- app/src/main/java/com/weheartit/challenge/MainActivity.java
+++ app/src/main/java/com/weheartit/challenge/MainActivity.java
@@ -25,9 +25,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class MainActivity extends Activity {
-    
-	ListView list;
-	
+
+    ListView list;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
@@ -42,71 +42,71 @@ public class MainActivity extends Activity {
                 .build());
 
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.main);
-        
-        list = (ListView)findViewById(android.R.id.list); 
-        
+
+        list = (ListView)findViewById(android.R.id.list);
+
         // To up to github and get the most recent rails commits and show them to the screen. 
-        DefaultHttpClient client = new DefaultHttpClient(); 
-		HttpGet get = new HttpGet("https://api.github.com/repos/rails/rails/commits");
-		HttpResponse response = null; 
-		try {
-			response = client.execute(get);
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		ArrayList<CommitModel> model = new ArrayList<>();
-		if(response.getStatusLine().getStatusCode() == 200) {
-			InputStream stream = null;
-			try {
-				stream = response.getEntity().getContent();
-			} catch (IllegalStateException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			String json = new Scanner(stream).useDelimiter("\\A").next();
-			model = new Gson().fromJson(json, new TypeToken<ArrayList<CommitModel>>(){}.getType());
-		}
-        
-		list.setAdapter(new CommitAdapter(model));
-		
-     
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpGet get = new HttpGet("https://api.github.com/repos/rails/rails/commits");
+        HttpResponse response = null;
+        try {
+            response = client.execute(get);
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        ArrayList<CommitModel> model = new ArrayList<>();
+        if(response.getStatusLine().getStatusCode() == 200) {
+            InputStream stream = null;
+            try {
+                stream = response.getEntity().getContent();
+            } catch (IllegalStateException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            String json = new Scanner(stream).useDelimiter("\\A").next();
+            model = new Gson().fromJson(json, new TypeToken<ArrayList<CommitModel>>(){}.getType());
+        }
+
+        list.setAdapter(new CommitAdapter(model));
+
+
     }
-    
+
     public class CommitAdapter extends ArrayAdapter<CommitModel> {
-    	
-    	List<CommitModel> commits;
-    	
-    	public CommitAdapter(List<CommitModel> commits) {
-    		super(MainActivity.this, android.R.layout.simple_expandable_list_item_2, commits);
-    		this.commits = commits; 
-    	}
-    	
-    	@Override
-    	public View getView(int position, View convertView, ViewGroup parent) {
-    		
-    		if(convertView == null) 
-    			convertView = LayoutInflater.from(MainActivity.this).inflate(android.R.layout.simple_expandable_list_item_2, null);
-    		
-    		CommitModel commit = commits.get(position);
-    		
-    		TextView top = (TextView) convertView.findViewById(android.R.id.text1);
+
+        List<CommitModel> commits;
+
+        public CommitAdapter(List<CommitModel> commits) {
+            super(MainActivity.this, android.R.layout.simple_expandable_list_item_2, commits);
+            this.commits = commits;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if(convertView == null)
+                convertView = LayoutInflater.from(MainActivity.this).inflate(android.R.layout.simple_expandable_list_item_2, null);
+
+            CommitModel commit = commits.get(position);
+
+            TextView top = (TextView) convertView.findViewById(android.R.id.text1);
             TextView bottom = (TextView) convertView.findViewById(android.R.id.text2);
-    		
-    		top.setText(commit.commit.author.name);
+
+            top.setText(commit.commit.author.name);
             bottom.setText(commit.commit.message);
-    		
-    		return convertView; 
-    	}
+
+            return convertView;
+        }
     };
-    
+
 }
