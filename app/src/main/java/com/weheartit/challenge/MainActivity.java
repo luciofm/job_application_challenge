2870dee28790552eb68de007ac84e9ffca5cc0b5
zhixu <k.zhiyuan.xu@gmail.com>
2014-12-23 17:56:12 -0800

    removed unnecessary import

--- app/src/main/java/com/weheartit/challenge/MainActivity.java
+++ app/src/main/java/com/weheartit/challenge/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
